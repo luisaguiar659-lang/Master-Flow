f5dbfaf071d22412dc4d4b48ec9e6b6744e1d7c0
luisaguiar659-lang <luis.aguiar659@gmail.com>
2026-09-13 03:09:07 -0300

    Create Android MainActivity base

---
+++ app/src/main/java/com/masterflow/app/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.masterflow.app
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Base inicial do Master-Flow.
+        // Os módulos serão conectados em etapas futuras.
+    }
+}

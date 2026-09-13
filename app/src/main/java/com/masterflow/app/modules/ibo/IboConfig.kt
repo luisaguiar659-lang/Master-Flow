f712507ee3b7a0877b21b8b54403c6d111339050
luisaguiar659-lang <luis.aguiar659@gmail.com>
2026-09-13 15:37:34 -0300

    Add IBO module configuration

---
+++ app/src/main/java/com/masterflow/app/modules/ibo/IboConfig.kt
@@ -0,0 +1,12 @@
+package com.masterflow.app.modules.ibo
+
+object IboConfig {
+    const val BASE_URL = "https://gerenciaapp.top/"
+    const val DASHBOARD_URL = "https://gerenciaapp.top/dashboard"
+    const val MODULE_VERSION = "1.2.3"
+
+    val allowedHosts = setOf(
+        "gerenciaapp.top",
+        "www.gerenciaapp.top"
+    )
+}

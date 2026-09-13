6fb781e2cae32f04f9956ff7715b74c405c650a7
luisaguiar659-lang <luis.aguiar659@gmail.com>
2026-09-13 16:02:13 -0300

    Use base URL as IBO login entry

--- app/src/main/java/com/masterflow/app/modules/ibo/IboConfig.kt
+++ app/src/main/java/com/masterflow/app/modules/ibo/IboConfig.kt
@@ -2,8 +2,8 @@ package com.masterflow.app.modules.ibo
 
 object IboConfig {
     const val BASE_URL = "https://gerenciaapp.top/"
+    const val LOGIN_URL = BASE_URL
     const val PANEL_URL = "https://gerenciaapp.top/dashboard"
-    const val LOGIN_URL = "https://gerenciaapp.top/login"
     const val DASHBOARD_URL = PANEL_URL
     const val MODULE_VERSION = "1.2.3"
 

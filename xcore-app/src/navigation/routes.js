455f65ee7aed61b00d22633113623d7a81d4e4e2
luisaguiar659-lang <luis.aguiar659@gmail.com>
2026-09-14 01:02:06 -0300

    feat: add XCORE app navigation structure

---
+++ xcore-app/src/navigation/routes.js
@@ -0,0 +1,6 @@
+export const routes = [
+  { name: 'Início', screen: 'Dashboard' },
+  { name: 'Gatilhos', screen: 'Triggers' },
+  { name: 'Mensagens', screen: 'Messages' },
+  { name: 'Config', screen: 'Settings' }
+];

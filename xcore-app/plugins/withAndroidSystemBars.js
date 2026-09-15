9b63bfe1716483394c2facfbb326c78f413d993a
luisaguiar659-lang <luis.aguiar659@gmail.com>
2026-09-14 23:46:36 -0300

    Use Expo config plugins package for Android system bars

--- xcore-app/plugins/withAndroidSystemBars.js
+++ xcore-app/plugins/withAndroidSystemBars.js
@@ -1,4 +1,4 @@
-const { withAndroidStyles } = require('expo/config-plugins');
+const { withAndroidStyles } = require('@expo/config-plugins');
 
 function upsertItem(style, name, value) {
   style.item = style.item || [];
